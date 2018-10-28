89b36af45c91b53c5c61d66b8e6f620d3f39fc74
Evan Lane <evanklane@yahoo.com>
2018-10-28 19:45:59 -0400

    added responsiveness to menu

--- menu.js
+++ menu.js
@@ -74,10 +74,61 @@ function toggleDrinks(){
   drinksClick.classList.toggle('open');
 }
 
+if(window.innerWidth >= 800){
+  drinksClick.classList.add('open');
+  dinnerClick.classList.add('open');
+  brunchClick.classList.add('open');
+  lunchClick.classList.add('open');
+  breakfastClick.classList.add('open');
+  drinksMenu.style.display = ""
+  dinnerMenu.style.display = ""
+  brunchMenu.style.display = ""
+  lunchMenu.style.display = ""
+  breakfastMenu.style.display = ""
+}
+
+
+function responsive(){
+if(window.innerWidth >= 800){
+  drinksClick.classList.add('open');
+  dinnerClick.classList.add('open');
+  brunchClick.classList.add('open');
+  lunchClick.classList.add('open');
+  breakfastClick.classList.add('open');
+  drinksMenu.style.display = ""
+  dinnerMenu.style.display = ""
+  brunchMenu.style.display = ""
+  lunchMenu.style.display = ""
+  breakfastMenu.style.display = ""
+//     breakfastClick.removeEventListener("click", toggleBreakfast)
+// lunchClick.removeEventListener("click", toggleLunch)
+// brunchClick.removeEventListener("click", toggleBrunch)
+// dinnerClick.removeEventListener("click", toggleDinner)
+// drinksClick.removeEventListener("click", toggleDrinks)
+}
+
+if(window.innerWidth <= 800){
+  drinksClick.classList.remove('open');
+  dinnerClick.classList.remove('open');
+  brunchClick.classList.remove('open');
+  lunchClick.classList.remove('open');
+  breakfastClick.classList.remove('open');
+  drinksMenu.style.display = "none"
+  dinnerMenu.style.display = "none"
+  brunchMenu.style.display = "none"
+  lunchMenu.style.display = "none"
+  breakfastMenu.style.display = "none"
+}
+}
+
 breakfastClick.addEventListener("click", toggleBreakfast)
 lunchClick.addEventListener("click", toggleLunch)
 brunchClick.addEventListener("click", toggleBrunch)
 dinnerClick.addEventListener("click", toggleDinner)
 drinksClick.addEventListener("click", toggleDrinks)
 
+
+
+
+window.addEventListener("resize", responsive)
 
